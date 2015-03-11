fd709885d7f8ad3fbd2b2c5eb5db6ccbef4b312c
chengz <chengz@staff.l99.com>
2015-03-11 18:14:01 +0800

    add la

--- app.js
+++ app.js
@@ -5,9 +5,11 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , user = require('./routes/user/user')
+  , user = require('./routes/word/word')
+  , user = require('./routes/note/note');
 
 var app = express();
 
@@ -28,7 +30,17 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+
+app.get('/word/:name', word.get);
+app.post('/word/:name', word.post);
+
+app.get('/note/:name', note.get);
+app.post('/note/:name', note.post);
+
+app.get('/user/:name', user.get);
+app.post('/user/:name', user.post);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/note/note.js
@@ -0,0 +1,27 @@
+
+
+// 保存单词本
+function save(){
+	
+}
+
+// 总入口. 命令列表, 定义了这个js可以处理的方法
+var coms={
+    save: save
+};
+
+// post方法总入口 
+function post(req, res){
+    var name = req.params.name;
+    
+    if(coms[name]){
+        coms[name](req, res);
+    }else{
+    	res.send("404 - page not find!");
+    }
+}
+// get 与post用同一套方法 
+exports.get = function(req, res){
+    post(req, res);
+};
+exports.post = post;
